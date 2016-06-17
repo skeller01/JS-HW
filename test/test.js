50fb0e68773c4e8b991c6f660e21810140f63f60
Steve <sfk55@cornell.edu>
2016-06-17 16:21:22 -0400

    Add two variables and sum them

--- test/test.js
+++ test/test.js
@@ -1,3 +1,11 @@
 //comment 1 
+// create two new variables 
+var x = 20; 
+var y = 100; 
+//store the top two variables, x and y, as a sum in a third 
+var z = x+y;
 
-Hi
+//Console log the two variables and their sum 
+console.log('x is'+' '+x)
+console.log('y is'+' '+y)
+console.log('The sum is'+' '+z);
