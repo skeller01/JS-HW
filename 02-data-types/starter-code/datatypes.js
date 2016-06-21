6023d65831f3b19d76e0782a0e152615ed1f0b03
skeller01 <losingmyhead@yahoo.com>
2016-06-21 19:58:49 -0400

    Finish in-class exercise on data types

--- 02-data-types/starter-code/datatypes.js
+++ 02-data-types/starter-code/datatypes.js
@@ -22,7 +22,7 @@ diff_y = (y2 - y1)*(y2 - y1);
 distance = Math.sqrt(diff_x+diff_y);
 
 console.log(distance);
-console.log("3^10 = "+ Math.pow(3,10))
+console.log("3^10 = "+ Math.pow(3,10));
 
 
 /**
@@ -36,8 +36,8 @@ console.log("3^10 = "+ Math.pow(3,10))
 
 var radius = 5;
 // Your code here
-sphere = (4/3)*Math.PI*Math.pow(radius,3)
-console.log(sphere)
+sphere = (4/3)*Math.PI*Math.pow(radius,3);
+console.log(sphere);
 
 /**
  * Question 3
@@ -47,7 +47,9 @@ console.log(sphere)
  */
 
 // Your code here
-
+for(i=1;i<100;i++){
+console.log(Math.random()*(100-50)+50);
+}
 
 /**
  * Question 4
@@ -57,9 +59,11 @@ console.log(sphere)
  *
  */
 
- var firstName = '';
+ var firstName = 'Steve';
+ var lastName = 'Keller';
+ var age = '33';
 // Your code here
-
+console.log(firstName+" is an amazing "+lastName+" and he's only "+age+" years old!");
 /**
  * Question 5
  *
@@ -67,9 +71,9 @@ console.log(sphere)
  * YELLING!
  *
  */
-
+var sentence = firstName+" is an amazing "+lastName+" and he's only "+age+" years old!";
 // Your code here
-
+console.log(sentence.toUpperCase());
 /**
  * Question 6
  *
@@ -81,4 +85,7 @@ console.log(sphere)
  */
 
 var firstPresWashington = true;
+var fiftyTwoStates = false;
+var capWashDC = true; 
 // Your code here
+console.log(firstPresWashington&&fiftyTwoStates||capWashDC);
