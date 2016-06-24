10040b9b9ef24ed4f040b1d9414b07a66b653581
skeller01 <losingmyhead@yahoo.com>
2016-06-23 20:34:46 -0400

    Start random address homework

---
+++ 03-conditionals-and-loops/assignments/address.js
@@ -0,0 +1,18 @@
+// Random Address Generator (address.js)
+// Write a script that can generate random addresses
+// As a first step, create arrays that contain dummy data for each of the following: street number, street name, city name, state name and zip code
+// Your script should randomly select one item from each of these arrays and then use them to construct a random address
+// Each time you run the script, it should print a new randomly-generated address to the terminal. For example:
+// node random-address.js
+// => 34578 Dolphin Street, Wonka NY, 44506
+// BONUS: What day? (day.js)
+// Write a switch statement to log to the console the appropriate day name for a given number.
+// 0: Sunday
+// 1: Monday
+// 2: Tuesday
+// 3: Wednesday
+// 4: Thursday
+// 5: Friday
+// 6: Saturday
+// Write a second switch statement to log 'Weekday' or 'Weekend' based on a given number. Use fall-through.
+
