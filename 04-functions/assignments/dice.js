138db35c7810dbfe9487efd707d76fe87e612a52
skeller01 <losingmyhead@yahoo.com>
2016-06-28 19:09:16 -0400

    Add assignments from starter code

---
+++ 04-functions/assignments/dice.js
@@ -0,0 +1,17 @@
+/*
+ *
+ * Question 1
+ * Write a function that will "roll a dice". It should generate a random number between 1 and 6 and then log that to the console.
+ * Call your function.
+ *
+ */
+
+
+
+/*
+ *
+ * Question 2
+ * Write a function that will roll two dice. In addition to logging each dice's value, it should also log the sum.
+ * Call your function.
+ *
+ */
---
+++ 04-functions/assignments/functionPractice.js
@@ -0,0 +1,66 @@
+/*
+ *
+ * Question 1
+ * Write a function that logs a random day of the week (i.e. 'Thursday')
+ *
+ */
+
+
+/*
+*
+* Question 2
+* Write a function that has two numbers parameters and multiplies them together.
+*
+*/
+
+
+/*
+*
+* Question 3
+* Write a function that takes an Array of numbers as an and logs the largest number in the Array.
+*
+*/
+
+
+
+/*
+*
+* Question 5
+* Write a function that takes a String and logs the String reversed.
+* Hint: Look up "split"
+*
+*/
+
+
+/*
+*
+* Question 6
+* Write a function that takes a given measurment in pounds to ounces. Return the result.
+*
+*/
+
+
+/*
+*
+* Question 7
+* Write a function that takes a given measurment in pounds to grams.
+* Use the function from Question 6 to first convert to ounces. Return the result.
+*
+*/
+
+
+/*
+*
+* Question 8
+* Write a function takes unlimited string parameters and returns them all concatenated together with a space in between each one.
+* Hint: Use function "arguments" variable
+*
+*/
+
+
+/*
+*
+* Question 9
+* Write a higher-order function takes takes a function paramter and calls it.
+*
+*/
