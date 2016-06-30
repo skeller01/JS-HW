bf70fdaa7647286a94ca28e1ffad3f70a5f30b4b
Steve <sfk55@cornell.edu>
2016-06-29 22:48:03 -0400

    Finish function practice from week 4

--- 04-functions/assignments/functionPractice.js
+++ 04-functions/assignments/functionPractice.js
@@ -23,14 +23,30 @@ function multNumbers(a,b){
 	return a*b; 
 }
 
+console.log("5 and 4 "+multNumbers(5,4));
+
 /*
 *
 * Question 3
 * Write a function that takes an Array of numbers as an and logs the largest number in the Array.
 *
 */
+bigList = [1,3,4,30,5,10,9];
+
+function findMax(x){
+var bigNumber = x[0]
+for(i=1;i<x.length;i++){
+	if(bigNumber>x[i]){
+		continue;
+	}else{
+		bigNumber=x[i];
+	}
+	}
+	return bigNumber;
+}
 
 
+console.log(findMax(bigList));
 
 /*
 *
@@ -39,15 +55,27 @@ function multNumbers(a,b){
 * Hint: Look up "split"
 *
 */
+var sentence = "Hi, I love this shit";
+function reverseTheSentence(sentence){
+newSentence = sentence.split("");
+revereseSentence = newSentence.reverse();
+finalSentence= revereseSentence.join("");
+console.log(finalSentence);
+}
 
-
+reverseTheSentence(sentence);
 /*
 *
 * Question 6
 * Write a function that takes a given measurment in pounds to ounces. Return the result.
 *
 */
+function lbsToOzs(pounds){
+	ounces=pounds*16;
+	return ounces;
+}
 
+console.log("5 lbs = "+lbsToOzs(5)+"ozs");
 
 /*
 *
@@ -56,7 +84,13 @@ function multNumbers(a,b){
 * Use the function from Question 6 to first convert to ounces. Return the result.
 *
 */
+function poundsToGrams(pounds){
+	ounces = lbsToOzs(pounds);
+	grams = 28.3495*ounces;
+	return grams;
+}
 
+console.log("4 ounces = "+poundsToGrams(4)+"grams");
 
 /*
 *
@@ -65,7 +99,18 @@ function multNumbers(a,b){
 * Hint: Use function "arguments" variable
 *
 */
+function bringTogether(){
+	var sentence = "";
+	for(i in arguments){
+		//console.log(arguments[i]);
+		sentence=sentence+arguments[i];
+		sentence=sentence+" ";
+	}
+	return sentence;
+	
+}
 
+console.log(bringTogether("f","44","y"));
 
 /*
 *
