c86879f816c10443f509d5238685d8b51705e4ff
skeller01 <losingmyhead@yahoo.com>
2016-06-30 19:08:06 -0400

    work on dataModel.js

--- 05-objects/starter-code/dataModel.js
+++ 05-objects/starter-code/dataModel.js
@@ -2,9 +2,26 @@
  * Question 1
  * Let's create a data model for storing information about a class.
  */
+var classInfo = {
+	students:30,
+	name:"Javascript",
+	teacher:"Jacob",
+	cost:4000
 
+};
+
+console.log(classInfo.teacher);
+classInfo.cost=3500;
+console.log(classInfo.cost);
 
 /**
  * Question 2
  * Create a data model for a quiz for our class.
  */
+var quizScience={
+	question1: "C02 = ?",
+	question2: "difhifhdhsfh",
+	question3:"odfudfuiuf",
+	totalPoints:100
+};
+
