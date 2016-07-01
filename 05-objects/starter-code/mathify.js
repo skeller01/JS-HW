d7d92ab249ba6a79aa4ad53df0817f8683cfe2f0
skeller01 <losingmyhead@yahoo.com>
2016-06-30 20:37:44 -0400

    finish mathify work from class

--- 05-objects/starter-code/mathify.js
+++ 05-objects/starter-code/mathify.js
@@ -7,3 +7,35 @@
  * Mathify should also have functions for add, subtract, divide and multiply. Each function should take
  * two parameters and return the result of the operation.
  */
+var mathify = {
+	"one":1,
+	"two":2,
+	"three":3,
+	"four":4,
+	"five":5,
+	"six":6,
+	"seven":7,
+	"eight":8,
+	"nine":9,
+	"ten":10
+
+};
+
+mathify.add = function(a,b){
+	var answer = a+b;
+	return answer;
+};
+
+mathify.subtract = function(a,b){
+	return a -b ;
+};
+
+mathify.divide = function(a,b){
+	return a/b;
+};
+
+mathify.multiply = function(a,b){
+	return a*b; 
+};
+
+console.log(mathify.add(mathify.one,mathify.four));
