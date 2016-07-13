62c247fd2ea92636a79d0ce6a74ed58a67bdd358
skeller01 <losingmyhead@yahoo.com>
2016-07-12 21:19:45 -0400

    begin the class work on MVC

---
+++ 08-events-and-templating/starter-code/todo-mvc/js/index.js
@@ -0,0 +1,10 @@
+//++++++++++++++++++++In class code-along for model view controller++
+
+//model 
+var todos=[
+	'Hello',
+	'World'
+];
+
+//view 
+//want to use templates and, therefore, libraries 
