86ae68cfc3e0e860f274e7d4e5fcdc8d749beddd
skeller01 <losingmyhead@yahoo.com>
2016-08-02 20:20:49 -0400

    finish in-class work on the review

--- 14-firebase/starter-code/hw-review.js
+++ 14-firebase/starter-code/hw-review.js
@@ -1,7 +1,29 @@
 // ## 1. Given then following API, create a model object with mock data.
 //
+var Model = {
+	token: "kjksdhw232j",
+	scores:[
+	{
+        "points": 14,
+        "user": "john",
+        "winning": true
+    },
+    {
+        "points": 10,
+        "user": "jane",
+        "winning":false
+       }
+	],
+	currentGame:{
+		{
+	    "name": "Test Game",
+	    "roundsPlayed": 2,
+	    "leader": "john"
+		}
+	}
+};
 // **GET /login**
-//
+//\
 // *Response Body*
 //
 // ```json
@@ -56,14 +78,37 @@
 //         <button class="plus-one">+</button>
 //     </li>
 // </ul>
-// ```
+
+<script id="winning-template" type="text/x-handlebars-template">
+	    {{#if token}}
+          
+			<ul class="standings">
+				{{#each scores}}
+				    <li class="{{#if winning}}winning{{/if}}">
+				        <h3>{{user}}</h3>
+				        <h4>{{points}} points</h4>
+				        <button class="plus-one">+</button>
+				    </li>
+				 
+				{{/each}} 
+			</ul>
+         
+       {{/if}}
+ // </script>
 
 
 // 3. Attach a fictional event listener to the `<button>`s in the created template that would, in theory, update the score.
-
+// $('.plus-one').on('click',updateScore);
+// OR
+$('body').on('click','.plus-one',updateScore)
+// This delegated listener will work well because we DON"T know the html yet but we're reasonably sure there's a body
 
 // 4. Write an AJAX request to send and receive fake data to the following endpoint and log response body's "status":
 // 
+
+$.post('/'+userName+'/score',{"points":20},function(data){
+	console.log(data.status);
+});
 // **PUT /:userName/score**
 //
 // *Request Body*
@@ -86,6 +131,15 @@
 
 // 5. Refactor the following AJAX request to use `$.ajax` instead of `$.post`.
 //
+$.ajax({
+	type:'POST',
+	url:'/game',
+	success: function(data){
+		console.log(data.leader);
+	},
+	data: newGame
+});
+
 // ```js
 // var newGame = {
 //     name: 'My New Game',
