9b28996ec107765ee69274b18dacf1fc99ad3127
skeller01 <losingmyhead@yahoo.com>
2016-08-11 20:45:38 -0400

    add final answer

--- 17-oop-and-review/starter-code/review.js
+++ 17-oop-and-review/starter-code/review.js
@@ -1,15 +1,25 @@
 // 1. Write an if/else conditional to assign the value of the description to 'even'
 // if the random number is even and 'odd' if the number is odd. Use an equality check.
-var randomNumber = Math.floor(Math.random() * 100);
-var description;
+// var randomNumber = Math.floor(Math.random() * 100);
+// var description;
 
-// 2. Refactor your answer from number 1 to use truthiness/falsiness in place of the
-// equality check.
+// if(randomNumber%2===0){
+// 	console.log('even');
+// }else{
+//  	console.log('odd');
+// }
 
+// // 2. Refactor your answer from number 1 to use truthiness/falsiness in place of the
+// // equality check.
+// if(randomNumber%2){
+// 	console.log('odd');
+// }else{
+//  	console.log('even');
+// }
 
-// 3. Refactor your answer from question 2 to use a ternary instead of an if/else
-// conditional.
-
+// // 3. Refactor your answer from question 2 to use a ternary instead of an if/else
+// // conditional.
+// (randomNumber%2===0)?'even' :'odd';
 
 // 4. Use a nested for loop to log whether each number is even or
 // odd in the nested arrays.
@@ -19,3 +29,15 @@ var nestedArrays = [
   [10, 32, 67, 12],
   [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)]
 ];
+
+for(i in nestedArrays){
+	for(j in nestedArrays[i]){
+		if(nestedArrays[i][j]%2===0){
+			console.log('even');
+			 // console.log(j);
+		}else{
+			console.log('odd')
+			// console.log(j);
+		}
+	}
+}
