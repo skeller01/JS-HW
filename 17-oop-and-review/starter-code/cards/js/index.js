02659b8153388d4684e0de40dbc86ce12dceef3f
skeller01 <losingmyhead@yahoo.com>
2016-08-11 20:18:41 -0400

    solve classwork

--- 17-oop-and-review/starter-code/cards/js/index.js
+++ 17-oop-and-review/starter-code/cards/js/index.js
@@ -1,101 +1,99 @@
-var values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
-var suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-
-function makeCard(value, suit) {
-  return {
-    value: value,
-    suit: suit,
-    getValue: function() {
-      return values[this.value];
-    },
-    getSuit: function() {
-      return suits[this.suit];
-    },
-    getName: function() {
-      return this.getValue() + ' of ' + this.getSuit();
-    },
-    getImage: function() {
-      return 'img/' + this.getValue().toLowerCase() + '_of_' + this.getSuit().toLowerCase() + '.png';
-    }
-  };
+function Card(value, suit) {
+  this.value = value;
+  this.suit = suit;
 }
 
-function makeDeck() {
-  var cards = [];
+Card.prototype.values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
 
-  for (var s = 0; s < 4; s++) {
-    for (var v = 0; v < 13; v++) {
-      cards.push(makeCard(v, s));
-    }
-  }
+Card.prototype.suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
 
-  return {
-    cards: cards,
-    shuffle: function() {
-      for (var i = this.cards.length - 1; i > 0; i -= 1) {
-        var j = Math.floor(Math.random() * (i + 1))
-        var temp = this.cards[i]
-        this.cards[i] = this.cards[j]
-        this.cards[j] = temp
-      }
-    },
-    drawCard: function() {
-      return this.cards.pop();
-    }
-  };
+Card.prototype.getValue =  function() {
+  return this.values[this.value];
 }
 
-function makeGame() {
-  var deck = makeDeck();
-  deck.shuffle();
+Card.prototype.getSuit = function() {
+  return this.suits[this.suit];
+}
+Card.prototype.getName = function() {
+  return this.getValue() + ' of ' + this.getSuit();
+}
+Card.prototype.getImage = function() {
+  return 'img/' + this.getValue().toLowerCase() + '_of_' + this.getSuit().toLowerCase() + '.png';
+}
 
-  return {
-    deck: deck,
-    playerScore: 0,
-    computerScore: 0,
-    playerCard: undefined,
-    computerCard: deck.drawCard(),
-    winner: undefined,
-    draw: function() {
-      this.playerCard = this.deck.drawCard();
 
-      if (this.playerCard.value > this.computerCard.value) {
-        this.playerScore++;
-      } else if (this.playerCard.value !== this.computerCard.value) {
-        this.computerScore++;
-      }
-    },
-    nextTurn: function() {
-      if (this.deck.cards.length === 0) {
-        if (this.playerScore > this.computerCard) {
-          this.winner = 'You Win!';
-        } else if (this.playerScore === this.computerScore) {
-          this.winner = 'Tie!';
-        } else {
-          this.winner = 'You lose :(';
-        }
-      } else {
-        this.playerCard = undefined;
-        this.computerCard = this.deck.drawCard();
-      }
-    },
-    getGameState: function() {
-      return {
-        cardsLeft: this.deck.cards.length,
-        playerCardImage: typeof this.playerCard === 'object' ? this.playerCard.getImage() : undefined,
-        playerCardName: typeof this.playerCard === 'object' ? this.playerCard.getName() : undefined,
-        playerScore: this.playerScore,
-        computerCardImage: this.computerCard.getImage(),
-        computerCardName: this.computerCard.getName(),
-        computerScore: this.computerScore,
-        winner: this.winner
-      };
-    }
-  };
+// var values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+// var suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+
+// function makeCard(value, suit) {
+//   return {
+//     value: value,
+//     suit: suit,
+//     getValue: function() {
+//       return values[this.value];
+//     },
+//     getSuit: function() {
+//       return suits[this.suit];
+//     },
+//     getName: function() {
+//       return this.getValue() + ' of ' + this.getSuit();
+//     },
+//     getImage: function() {
+//       return 'img/' + this.getValue().toLowerCase() + '_of_' + this.getSuit().toLowerCase() + '.png';
+//     }
+//   };
+// }
+
+function Game(){
+  this.deck = new Deck();
+  this.deck.shuffle();
+  this.playerScore = 0,
+  this.computerScore = 0,
+  this.playerCard = undefined,
+  this.computerCard = this.deck.drawCard(),
+  this.winner = undefined
+}
+
+Game.prototype.draw = function() {
+  this.playerCard = this.deck.drawCard();
+  if (this.playerCard.value > this.computerCard.value) {
+    this.playerScore++;
+  } else if (this.playerCard.value !== this.computerCard.value) {
+    this.computerScore++;
+  }
+}
+
+Game.prototype.nextTurn = function() {
+  if (this.deck.cards.length === 0) {
+    if (this.playerScore > this.computerCard) {
+      this.winner = 'You Win!';
+    } else if (this.playerScore === this.computerScore) {
+      this.winner = 'Tie!';
+    } else {
+      this.winner = 'You lose :(';
+    }
+  } else {
+    this.playerCard = undefined;
+    this.computerCard = this.deck.drawCard();
+  }
+}
+
+Game.prototype.getGameState = function() {
+  return {
+    cardsLeft: this.deck.cards.length,
+    playerCardImage: typeof this.playerCard === 'object' ? this.playerCard.getImage() : undefined,
+    playerCardName: typeof this.playerCard === 'object' ? this.playerCard.getName() : undefined,
+    playerScore: this.playerScore,
+    computerCardImage: this.computerCard.getImage(),
+    computerCardName: this.computerCard.getName(),
+    computerScore: this.computerScore,
+    winner: this.winner
+  }
 }
 
 // Model
-var model = makeGame();
+var model = new Game();
+
 
 // View
 
